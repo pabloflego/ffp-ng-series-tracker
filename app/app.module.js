94480b8bad4fe008f01b06cb8a507f829c3259a4
Pablo Flego <pablinff@gmail.com>
2016-04-26 03:41:48 -0300

    Initial Commit

---
+++ app/app.module.js
@@ -0,0 +1,22 @@
+(function () {
+    'use strict';
+
+    angular.module('app', [
+        /*
+         * Everybody has access to these.
+         * We could place these under every feature area,
+         * but this is easier to maintain.
+         */
+        // 'app.core',
+        // 'app.widgets',
+
+        /*
+         * Feature areas
+         */
+
+        'app.layout'
+        // 'app.avengers',
+        // 'app.dashboard',
+    ]);
+
+})();
